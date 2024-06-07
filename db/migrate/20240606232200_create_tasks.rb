9960cc88c2b2b15861d97cde9cb4406cdb7e31ca
isaac <isaac.brigido@gmail.com>
2024-06-06 22:01:02 -0300

    fix_2: Valor default para atividade finalizada

--- db/migrate/20240606232200_create_tasks.rb
+++ db/migrate/20240606232200_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
     create_table :tasks do |t|
       t.string :name, limit: 50
       t.string :description, limit: 140
-      t.boolean :completed, null: false
+      t.boolean :completed, null: false, default: false
       t.date :due_date
       t.integer :priority, null: false, default: 0
       t.references :member, null: false, foreign_key: true
