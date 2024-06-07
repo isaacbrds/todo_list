227727ae8a382b5047c4b0d792dc6a2666b41553
isaac <isaac.brigido@gmail.com>
2024-06-06 21:36:53 -0300

    feature_3: alteração no tamanho da senha do Membros

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,6 +6,6 @@ class Member < ApplicationRecord
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   validates :name, length: { minimum: 5 }
-
+  validates :password, length: { minimum: 3 }
   has_many :tasks
 end
