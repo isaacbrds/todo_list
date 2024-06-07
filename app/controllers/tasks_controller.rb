8057029eaa0bfb1bb35d4e6e27504f8b1f522d72
isaac <isaac.brigido@gmail.com>
2024-06-06 21:04:48 -0300

    feature_4: Criando a tela edição da tarefa

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    
+    before_action :set_task, only: %i[edit update]
     def new
         @task = Task.new
     end
@@ -18,8 +18,25 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit;end
+
+    def update
+        respond_to do |format|
+            if @task.update(task_params)
+              format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
+              format.json { render :show, status: :ok, location: @task }
+            else
+              format.html { render :edit, status: :unprocessable_entity }
+              format.json { render json: @task.errors, status: :unprocessable_entity }
+            end
+          end
+    end
+
     private
 
+    def set_task
+        @task = Task.find(params[:id])
+    end
     def task_params
         params.require(:task).permit(:name, :description, :completed, :priority)
     end
